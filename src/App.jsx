20698969bc7cfc92aa7f92607913508c547f32e3
krishna1608 <rishnak10@gmail.com>
2024-11-01 13:16:36 +0530

    cc

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import Landing from "./components/Landing"; 
-import Loadpage from "./components/Loadpage"; 
+import Landing from "./Components/Landing"; 
+import Loadpage from "./Components/Loadpage"; 
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css'
 
